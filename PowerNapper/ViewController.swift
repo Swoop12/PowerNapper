8c294f213dc9bb2b64249dc764f9f32fc66715f8
Trevor Adcock <trevor.adcock@me.com>
2018-09-04 10:25:34 -0600

    Initial Commit

---
+++ PowerNapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PowerNapper
+//
+//  Created by DevMountain on 9/4/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
